582cc667c74e79ca24afb4dc5dec5d429971f631
jmossesgeld <jmossesgeld@gmail.com>
2021-10-26 18:32:12 +0800

    change url source

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import requests
 import os
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
 
-def get_images_data_src(endpoint, title, chapter):
+def get_images(endpoint, title, chapter):
     try:
         os.makedirs(chapter)
     except:
@@ -17,26 +16,26 @@ def get_images_data_src(endpoint, title, chapter):
 
     reading_content = soup.find_all('div', class_="reading-content")[0]
     images = reading_content.find_all('img')
-    images_links = [i.get('data-src').strip() for i in images]
-    return images_links
+    images_links = [i.get('src').strip() for i in images]
+    img_count = len(images_links)
+    print(f"{img_count} image links retrieved.")
+    for idx, link in enumerate(images_links):
+        print(f"Downloading image [{idx + 1} / {img_count}]...")
+        img = requests.get(link, headers=header).content
+        with open(f'{chapter}/{idx}.jpg', 'wb') as file:
+            file.write(img)
+            print(f"Image saved succesfully.")
+    print("All images downloaded succesfully")
 
 
-endpoint = "https://toonily.com/webtoon"
-title = "touch-unlock-0001"
-chapter = "chapter-92"
+
+endpoint = "https://mangaforfree.net/manga"
+title = "touch-to-unlock-30"
+chapter = "chapter-96-eng"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
 }
 
-images_data_src = get_images_data_src(endpoint, title, chapter)
-
-def get_images(links):
-    chrome_driver_path = 'C:/chromedriver_win32/chromedriver.exe'
-    driver = webdriver.Chrome(chrome_driver_path)
-    driver.get(links[0])
-    driver.maximize_window()
-
-get_images(images_data_src)
-
+get_images(endpoint, title, chapter)
 
