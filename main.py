fb67f8c69e1bb3ff90b9589f210f2b07caaf08e4
jmossesgeld <jmossesgeld@gmail.com>
2021-11-14 20:04:33 +0800

    change log message

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 
 def get_images(url, next=False):
     ts = time.time()  # start timer
-    _, _, _, _, title, chapter, _ = url.split('/') # get directory names
+    _, _, _, _, title, chapter, _ = url.split('/')  # get directory names
     try:
-        os.makedirs(f'{title}/{chapter}') # create folders
+        os.makedirs(f'{title}/{chapter}')  # create folders
     except:
         print("Folder already exist.")
 
@@ -24,11 +24,14 @@ def get_images(url, next=False):
     print(f"{img_count} image links retrieved.")
 
     # download images
-    for idx, link in enumerate(images_links):
-        print(f"Downloading image [{idx + 1} / {img_count}]...")
-        img = requests.get(link, headers=header).content
-        with open(f'{title}/{chapter}/{idx+1}.jpg', 'wb') as file:
-            file.write(img)
+    if chapter.find('raw') == -1:
+        for idx, link in enumerate(images_links):
+            print(f"Downloading image [{idx + 1} / {img_count}]...")
+            img = requests.get(link, headers=header).content
+            with open(f'{title}/{chapter}/{idx+1}.jpg', 'wb') as file:
+                file.write(img)
+    else:
+        print("RAW chapter found. Skipping...")
     if next and next_url:
         get_images(next_url, True)
     else:
@@ -37,8 +40,8 @@ def get_images(url, next=False):
 
 
 endpoint = "https://mangaforfree.net/manga"
-title = "touch-to-unlock-30"
-chapter = "chapter-27"
+title = "touch-to-unlock-32"
+chapter = "chapter-84-eng"
 url = f"{endpoint}/{title}/{chapter}/"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
