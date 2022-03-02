09587d002a3d72c75b43db6261169e99befc6c05
jmossesgeld <jmossesgeld@gmail.com>
2022-03-03 00:03:16 +0800

    remove time logging

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 import requests
 import os
-import time
 from bs4 import BeautifulSoup
-from bs4.element import NavigableString, Tag, ResultSet
+from bs4.element import Tag, ResultSet
 from typing import Iterable, cast
 
-# CONSTANTS
-endpoint = "https://mangaforfree.net/manga"
-title = "touch-to-unlock-44"
-chapter = "chapter-1"
-url = f"{endpoint}/{title}/{chapter}/"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
 }
 
 
-def get_images(url: str, next=False):
-    # start timer log
-    ts = time.time()
-
-    # get directories
+def mangaforfree(url):
+    # get title and chapter from url
     _, _, _, _, title, chapter, _ = url.split('/')
 
     # request html
@@ -29,7 +20,8 @@ def get_images(url: str, next=False):
 
     # create soup
     soup = BeautifulSoup(html_doc, 'html.parser')
-    reading_content: Tag = soup.find_all('div', class_="reading-content")[0]
+    reading_content: Tag = soup.find_all(
+        'div', class_="reading-content")[0]
 
     # get link of next chapter
     try:
@@ -39,30 +31,31 @@ def get_images(url: str, next=False):
 
     # get link of images
     images: ResultSet = reading_content.find_all('img')
-    images_links = [i.get('src').strip() for i in cast(Iterable[Tag], images)]
+    images_links = [i.get('src').strip()
+                    for i in cast(Iterable[Tag], images)]
     img_count = len(images_links)
     print(f"{img_count} image links retrieved.")
 
     if chapter.find('raw') == -1:
         # create folders
         try:
-            os.makedirs(f'{title}/{chapter}')
+            os.makedirs(f'downloaded/{title}/{chapter}')
         except:
             print("Folder already exist.")
         # download images
         for idx, link in enumerate(images_links):
             print(f"Downloading image [{idx + 1} / {img_count}]...")
             img = requests.get(link, headers=header).content
-            with open(f'{title}/{chapter}/{idx+1}.jpg', 'wb') as file:
+            with open(f'downloaded/{title}/{chapter}/{idx+1}.jpg', 'wb') as file:
                 file.write(img)
     else:
         print("RAW chapter found. Skipping...")
     if next and next_url:
         # download next chapter by recursing this function
-        get_images(next_url, True)
+        mangaforfree(next_url, True)
     else:
         print(
-            f"All images downloaded succesfully ({(time.time() - ts).__floor__()}s). Program is exiting.")
+            f"All images downloaded succesfully. Program is exiting.")
 
 
-get_images(url, True)
+mangaforfree("https://mangaforfree.net/manga/touch-to-unlock-45/chapter-1/")
